e4ec794a7d626def85bed4270198037802766ba2
Ahmed Khaled Mohamed <100778273+Ahmed-Khaled-JS@users.noreply.github.com>
2024-04-28 21:16:19 +0300

    first

--- src/views/Main.js
+++ src/views/Main.js
@@ -5,55 +5,58 @@ import Header from "../components/shared/Header";
 import { jwtDecode } from "jwt-decode"; // Correct import
 import { useState, useEffect } from "react";
 import MainLayout from "../components/MainLayout";
-import axios  from '../../node_modules/axios/dist/esm/axios';
-import baseurl from '../apis';
+import axios from "../../node_modules/axios/dist/esm/axios";
+import baseurl from "../apis";
 import CatagorySection from "../components/CatagorySection";
 
 const Main = () => {
-    const [userRole, setUserRole] = useState("");
-    const [catagories,setCatagories] = useState([]);
-    useEffect(() => {
-        const tokendetails = async () => {
-            const token = sessionStorage.getItem('token'); // Assuming the token is stored in local storage
-            if (token && token !== "undefined") {
-                const decoded = jwtDecode(token);
-                setUserRole(decoded.role);
-            }
-            try {
-                const response = await axios.get(`${baseurl}/category`, {
-                    headers: {
-                      Authorization: sessionStorage.getItem("token"),
-                    },
-                  });
-                setCatagories(response.data.result);
-            } catch (error) {
-                console.log("eh ya she5a");
-            }
-            
-
-        }
-
-        tokendetails();
-    }, []);
-
-    const logout = () => {
-        sessionStorage.removeItem('token');
-        setUserRole("");
-    }
-
-    if (!sessionStorage.getItem('token') || sessionStorage.getItem('token') === "undefined") {
-        sessionStorage.removeItem('token');
+  const [userRole, setUserRole] = useState("");
+  const [catagories, setCatagories] = useState([]);
+  useEffect(() => {
+    const tokendetails = async () => {
+      const token = sessionStorage.getItem("token"); // Assuming the token is stored in local storage
+      if (token && token !== "undefined") {
+        const decoded = jwtDecode(token);
+        setUserRole(decoded.role);
+      }
+      try {
+        const response = await axios.get(`${baseurl}/category`, {
+          headers: {
+            Authorization: sessionStorage.getItem("token"),
+          },
+        });
+        setCatagories(response.data.result);
+      } catch (error) {
+        console.log("eh ya she5a");
+        sessionStorage.removeItem("token");
         return <Navigate to="/signIn" />;
-    }
+      }
+    };
 
-    return (
-        <>
-            <Header logout={logout} userRole={userRole} />
-            <MainLayout />
-            <CatagorySection catagories={catagories}/>
-            <Footer />
-        </>
-    );
-}
+    tokendetails();
+  }, []);
+
+  const logout = () => {
+    sessionStorage.removeItem("token");
+    setUserRole("");
+  };
+
+  if (
+    !sessionStorage.getItem("token") ||
+    sessionStorage.getItem("token") === "undefined"
+  ) {
+    sessionStorage.removeItem("token");
+    return <Navigate to="/signIn" />;
+  }
+
+  return (
+    <>
+      <Header logout={logout} userRole={userRole} />
+      <MainLayout />
+      <CatagorySection catagories={catagories} />
+      <Footer />
+    </>
+  );
+};
 
 export default Main;
--- src/views/Movies.js
+++ src/views/Movies.js
@@ -5,28 +5,55 @@ import baseurl from "../apis";
 import Header from "../components/shared/Header";
 import Footer from "../components/shared/Footer";
 import { Link } from "react-router-dom";
+import { jwtDecode } from "jwt-decode"; // Correct import
+import { Navigate } from "react-router-dom";
+
 const Movies = () => {
   const [movies, setmovies] = useState([]);
   const [moviesname, setmoviesnaem] = useState([]);
-
+  const [userRole, setUserRole] = useState("");
   const id = useParams();
   const getdata = async () => {
+    const token = sessionStorage.getItem("token"); // Assuming the token is stored in local storage
+    if (token && token !== "undefined") {
+      const decoded = jwtDecode(token);
+      setUserRole(decoded.role);
+    }
     console.log(id);
-    const response = await axios.get(`${baseurl}/movie?id=${id.id}`,{
-      headers: {
-        Authorization: sessionStorage.getItem("token"),
-      },
-    });
-    setmovies(response.data.result);
-    setmoviesnaem(response.data.categoryName);
-    console.log(response.data);
+    
+    try {
+      const response = await axios.get(`${baseurl}/movie?id=${id.id}`, {
+        headers: {
+          Authorization: sessionStorage.getItem("token"),
+        },
+      });
+      setmovies(response.data.result);
+      setmoviesnaem(response.data.categoryName);
+      console.log(response.data);
+    } catch (error) {
+      console.log("eh ya she5a");
+      sessionStorage.removeItem("token");
+      return <Navigate to="/signIn" />;
+    }
   };
   useEffect(() => {
     getdata();
   }, []);
+  const logout = () => {
+    sessionStorage.removeItem("token");
+    setUserRole("");
+  };
+  if (
+    !sessionStorage.getItem("token") ||
+    sessionStorage.getItem("token") === "undefined"
+  ) {
+    sessionStorage.removeItem("token");
+    return <Navigate to="/signIn" />;
+  }
+
   return (
     <>
-      <Header />
+      <Header logout={logout} userRole={userRole} />
       <p class="text-center  textCategory" id="title">
         {moviesname}
       </p>
@@ -41,10 +68,7 @@ const Movies = () => {
               />
 
               <p class="text-center pt-3 pe-3">
-                <Link
-                  class="categoryTitle "
-                  to={`/moviedetails/${movie._id}`}
-                >
+                <Link class="categoryTitle " to={`/moviedetails/${movie._id}`}>
                   {movie.movieName}
                 </Link>
               </p>
